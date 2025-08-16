e433b010323c1002786a4070312dafb39b0a4bb1
giahuytruonn <zblackz1oooo@gmail.com>
2025-08-16 18:26:34 +0700

    First Init

---
+++ src/main/java/tourbooking/vietvivu/VietVivuApplication.java
@@ -0,0 +1,13 @@
+package tourbooking.vietvivu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietVivuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VietVivuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tourbooking/vietvivu/VietVivuApplicationTests.java
@@ -0,0 +1,13 @@
+package tourbooking.vietvivu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietVivuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
